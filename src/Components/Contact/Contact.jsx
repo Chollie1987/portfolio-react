75bbc8ff4603a09167f83fb13b0cfce8b75000e7
Christina Hollie <101164452+Chollie1987@users.noreply.github.com>
2024-10-20 12:16:54 -0700

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
         event.preventDefault();
         const formData = new FormData(event.target);
 
-        formData.append("access_key", "VITE_API_KEY");
+        formData.append("access_key", import.meta.env.VITE_API_KEY);
 
         const object = Object.fromEntries(formData);
         const json = JSON.stringify(object);
